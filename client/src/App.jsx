55ecc34412206aa2fe5f55c9e5afe4102077efa6
electricplayer <40640596+electricplayer@users.noreply.github.com>
2024-07-15 00:10:42 -0500

    Second Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
           <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 18.75 7.5-7.5 7.5 7.5" />
           <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 7.5-7.5 7.5 7.5" />
         </svg>
-        <span className='font-bold'>RentFree</span>
+        <span className='font-bold'>rentfree</span>
         </a>
         <div className='flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-500'>
           <div>Anywhere</div>
@@ -24,7 +24,7 @@ function App() {
           </svg>
           </button>
         </div>
-        <div className='flex gap-2 border border-gray-300 rounded-full py-2 px-4'>
+        <div className='flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4'>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
