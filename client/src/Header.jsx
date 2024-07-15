1c9cfec2bc776c8d95e60f8793b7738a5e5ddca6
electricplayer <40640596+electricplayer@users.noreply.github.com>
2024-07-15 01:02:54 -0500

    Sixth Update

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,5 +1,6 @@
+import {Link} from "react-router-dom";
 export default function Header() {
-    return (
+    return(
         <header className='p-3 flex justify-between'>
         <a href="" className="flex items-center gap-1">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10">
@@ -28,8 +29,6 @@ export default function Header() {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 relative top-1">
           <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
         </svg>
-
-
         </div>
         </Link>
       </header>
