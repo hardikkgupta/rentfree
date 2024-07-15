4ee4bffdfccc21b397386108a3f8d0ac596f3e2b
electricplayer <40640596+electricplayer@users.noreply.github.com>
2024-07-15 16:18:07 -0500

    Update Login Page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,3 +1,15 @@
 export default function LoginPage() {
-    return 'login form should go here'
+    return (
+        <div className='mt-4 grow flex items-center justify-around'>
+            <div className="mb-52">
+            <h1 className='text-4xl text-center mb-4'>Login</h1>
+            <form className='max-w-md mx-auto'>
+                <input type="email" placeholder='your@email.com'/>
+                <input type="password" placeholder="password"/>
+                <button className="primary">Login</button>
+            </form>
+            </div>
+            
+        </div>
+    );
 }
