fbdaafc9cc7b7d8dcef33672896fc94b734d9458
electricplayer <40640596+electricplayer@users.noreply.github.com>
2024-07-22 03:13:15 -0500

    Update Photo by Link functionality

--- api/index.js
+++ api/index.js
@@ -3,9 +3,10 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
-const CookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
 require('dotenv').config()
 const app = express();
 const bcryptSalt = bcrypt.genSaltSync(10);
@@ -13,6 +14,7 @@ const jwtSecret = 'ndksfnleadfldfaslladf';
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
     credentials: true, 
     origin: 'http://localhost:5173',
@@ -74,9 +76,19 @@ app.get('/profile', (req, res) =>{
     }
 })
 
-app.post('/logout', (req, res) => {
-    res.cookie('token', '').json(true);
-
+app.post('/upload-by-link', async (req, res) => {
+    const { link } = req.body;
+    const newName = 'photo' + Date.now() + '.jpg';
+    try {
+        await imageDownloader.image({
+            url: link,
+            dest: __dirname + '/uploads/' + newName,
+        });
+        res.json(newName);
+    } catch (error) {
+        console.error('Error downloading image:', error);
+        res.status(500).json({ error: 'Error downloading image' });
+    }
 });
 
 const PORT = process.env.PORT || 4000;
