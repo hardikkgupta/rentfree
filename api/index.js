422629b8f9a0da93182a35b63db7ff0d16c16478
electricplayer <40640596+electricplayer@users.noreply.github.com>
2024-07-24 01:34:59 -0500

    listing and editing places

--- api/index.js
+++ api/index.js
@@ -120,15 +120,46 @@ app.post('/places', (req, res) => {
     } = req.body;
     jwt.verify(token, jwtSecret, {}, async (err, userData) => {
         if (err) throw err;
-        const placeDoc = await Place.create({
-            owner: userData.id,
-            title, address, addedPhotos, description,
-        perks, extraInfo, checkIn, checkOut, maxGuests,
-        })
+        const placeDoc = await Place.create()
         res.json(placeDoc);
     });
     
 });
+
+app.get('/places', (req, res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+        const {id} = userData;
+        res.json(await Place.find({owner:id}));
+    });
+});
+
+app.get('/places/:id', async (req, res) => {
+    const {id} = req.params;
+    res.json(await Place.findById(id));
+})
+
+app.put('/places', async (req, res) => {
+    const {token} = req.cookies;
+    const {
+        id, title, address, addedPhotos, description,
+        perks, extraInfo, checkIn, checkOut, maxGuests,
+    } = req.body;
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+        if (err) throw err;
+        const placeDoc = await Place.findById(id);
+        if (userData.id === placeDoc.owner.toString()) {
+            placeDoc.set({
+                
+                title, address, photos:addedPhotos, description,
+                perks, extraInfo, checkIn, checkOut, maxGuests,
+            });
+            await placeDoc.save();
+            res.json('ok');
+        }
+    });
+});
+
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
--- client/src/pages/PlacesFormPage.jsx
+++ client/src/pages/PlacesFormPage.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Perks from "../Perks";
 import PhotosUploader from "../PhotosUploader";
 import axios from "axios";
 import AccountNav from "../AccountNav";
-import { Navigate } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 
 export default function PlacesFormPage() {
+    const {id} = useParams();
+
     const [title, setTitle] = useState('');
     const [address, setAddress] = useState('');
     const [description, setDescription] = useState('');
@@ -16,6 +18,23 @@ export default function PlacesFormPage() {
     const [checkOut, setCheckOut] = useState('');
     const [maxGuests, setMaxGuests] = useState(1);
     const [redirect, setRedirect] = useState(false);
+    useEffect(() => {
+        if (!id) {
+            return;
+        }
+        axios.get('/places/'+id).then(response => {
+            const {data} = response;
+            setTitle(data.title);
+            setAddress(data.address);
+            setAddedPhotos(data.photos);
+            setDescription(data.description);
+            setPerks(data.perks);
+            setExtraInfo(data.extraInfo);
+            setCheckIn(data.checkIn);
+            setCheckOut(data.checkOut);
+            setMaxGuests(data.maxGuests);
+        });
+    }, [id]);
 
     function inputHeader(text){
         return (
@@ -38,14 +57,22 @@ export default function PlacesFormPage() {
         );
     }
 
-    async function addNewPlace(ev) {
+    async function savePlace(ev) {
         ev.preventDefault();
-        await axios.post('/places', {
+        const placeData = {
             title, address, addedPhotos, 
             description, perks, extraInfo, 
             checkIn, checkOut, maxGuests
-        });
-        setRedirect(true);
+        };
+        if (id) {
+            await axios.put('/places', {
+                id, ...placeData
+            });
+            setRedirect(true);
+        } else {
+            await axios.post('/places', placeData);
+            setRedirect(true);
+        }
     }
 
     if (redirect) {
@@ -55,7 +82,7 @@ export default function PlacesFormPage() {
     return (
                 <div>
                     <AccountNav />
-                    <form onSubmit={addNewPlace}>
+                    <form onSubmit={savePlace}>
                         {preInput('Title', 'Tell us about your home')}
                         <input type="text" value={title} onChange={ev => setTitle(ev.target.value)} placeholder="title, for example: My apt"/>
                         {preInput('Address', 'Confirm your location')}
