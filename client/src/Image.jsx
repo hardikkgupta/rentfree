a2dbfb40de847d77e4eb7412e114c18e7f921c43
hardikkgupta <40640596+hardikkgupta@users.noreply.github.com>
2024-07-30 06:47:00 -0500

    Update Image.jsx

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,6 +1,6 @@
 export default function Image({src, ...rest}) {
     src = src && src.includes('https://') ? src 
-    : 'http://localhost:4000/uploads/'+src;
+    : 'https://rentfree-app.s3.amazonaws.com/uploads/'+src;
     return (
         <img {...rest} src={src} alt={''}/>
     );
